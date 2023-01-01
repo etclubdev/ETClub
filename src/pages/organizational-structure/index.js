cb29a75190764cba0735b2ae5494ea7f4e142625
thduy2003 <duytrieudong@gmail.com>
2023-01-01 19:38:04 +0700

    test remove lazy

--- src/pages/organizational-structure/index.js
+++ src/pages/organizational-structure/index.js
@@ -1,18 +1,17 @@
 import { Fragment, lazy, Suspense } from "react";
-
-const Content = lazy(() =>
-  import("../../components/organizational-structure/Content")
-);
-const Name = lazy(() =>
-  import("../../components/organizational-structure/Name")
-);
+import Content from "../../components/organizational-structure/Content";
+import Name from "../../components/organizational-structure/Name";
+// const Content = lazy(() =>
+//   import("../../components/organizational-structure/Content")
+// );
+// const Name = lazy(() =>
+//   import("../../components/organizational-structure/Name")
+// );
 const OrganizationalStructure = () => {
   return (
     <Fragment>
-      <Suspense>
-        <Name></Name>
-        <Content></Content>
-      </Suspense>
+      <Name></Name>
+      <Content></Content>
     </Fragment>
   );
 };
