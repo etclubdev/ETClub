18b7f11425c7ece876ed774e668aef2bb3014ed4
thduy2003 <duytrieudong@gmail.com>
2023-01-01 19:40:08 +0700

    return lazy

--- src/pages/organizational-structure/index.js
+++ src/pages/organizational-structure/index.js
@@ -1,17 +1,18 @@
 import { Fragment, lazy, Suspense } from "react";
-import Content from "../../components/organizational-structure/Content";
-import Name from "../../components/organizational-structure/Name";
-// const Content = lazy(() =>
-//   import("../../components/organizational-structure/Content")
-// );
-// const Name = lazy(() =>
-//   import("../../components/organizational-structure/Name")
-// );
+
+const Content = lazy(() =>
+  import("../../components/organizational-structure/Content")
+);
+const Name = lazy(() =>
+  import("../../components/organizational-structure/Name")
+);
 const OrganizationalStructure = () => {
   return (
     <Fragment>
-      <Name></Name>
-      <Content></Content>
+      <Suspense>
+        <Name></Name>
+        <Content></Content>
+      </Suspense>
     </Fragment>
   );
 };
