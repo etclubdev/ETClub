9174bf991e11c5131fba0b1d9179a06c4722ed64
thduy2003 <duytrieudong@gmail.com>
2023-01-02 00:35:58 +0700

    fix banner

--- src/components/homepage/Banner.js
+++ src/components/homepage/Banner.js
@@ -5,36 +5,45 @@ import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 import "./banner.scss";
 export default function Banner() {
+  const options = {
+    nav: true,
+    items: 1,
+    loop: true,
+    margin: 0,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 2000,
+    navSpeed: 2000,
+    navText: [
+      "<i class='fas fa-chevron-left'></i>",
+      "<i class='fas fa-chevron-right'></i>",
+    ],
+    stagePadding: 0,
+    responsive: {
+      0: {
+        nav: false,
+      },
+      1000: {
+        nav: true,
+      },
+    },
+  };
   const array = [1, 2, 3, 4, 5, 6, 8, 9, 10];
   return (
-    <div>
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        breakpoints={{
-          0: {
-            spaceBetween: 0,
-          },
-          800: {
-            spaceBetween: 50,
-          },
-        }}
-      >
-        {array.map((item) => (
-          <SwiperSlide key={item}>
+    <>
+      <OwlCarousel className="owl-theme owl-carousel-banner" {...options}>
+        {array.map((item, index) => (
+          <div className="item" key={index}>
             <img src="./img/banner.png" />
-          </SwiperSlide>
+          </div>
         ))}
-      </Swiper>
-    </div>
+      </OwlCarousel>
+    </>
   );
 }
