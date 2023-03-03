62ce90b839fdc924113d14bb57fed6ee9ac10964
Le Quan <lequan2002.ql@gmail.com>
2023-03-03 19:22:23 +0700

    db sript & re-set port b-e

---
+++ et_websie_db.sql
@@ -0,0 +1,67 @@
+DROP DATABASE IF EXISTS et_website_db;
+CREATE DATABASE et_website_db;
+CREATE TABLE feeling (
+  id INT NOT NULL AUTO_INCREMENT,
+  quote TEXT NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE basic_info (
+  operating_year INT NOT NULL,
+  media_channel INT NOT NULL,
+  workshop_talkshow INT NOT NULL
+);
+
+CREATE TABLE et_news (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  tiny_dest TEXT NOT NULL,
+  full_news TEXT NOT NULL,
+  view INT NOT NULL,
+  create_time DATETIME NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE competition (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  status VARCHAR(255) NOT NULL,
+  landscape_poster TEXT,
+  portrait_poster TEXT,
+  lookback_script TEXT,
+  lookback_img TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE milestone (
+  id INT NOT NULL AUTO_INCREMENT,
+  competition_id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (competition_id) REFERENCES competition(id)
+);
+
+CREATE TABLE competition_result (
+  id INT NOT NULL AUTO_INCREMENT,
+  competition_id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  major VARCHAR(255) NOT NULL,
+  academic_year VARCHAR(255) NOT NULL,
+  team VARCHAR(255),
+  rank INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (competition_id) REFERENCES competition(id)
+);
+
+CREATE TABLE sponsor (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  logo TEXT NOT NULL,
+  kind VARCHAR(255) NOT NULL,
+  competition_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (competition_id) REFERENCES competition(id)
+);
