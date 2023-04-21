e3d4150f0b1772925d6939a412ffe49a4b97d732
Tran Hoang Duy <duytrieudong@gmail.com>
2023-04-21 16:00:30 +0700

    gan api sposor + competition homepage

--- font-end/src/components/homepage/Competition.js
+++ font-end/src/components/homepage/Competition.js
@@ -4,6 +4,7 @@ import "./Competition.scss";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
+import competitionApi from "../../api/competitionApi";
 const Competition = () => {
   const options = {
     nav: true,
@@ -40,38 +41,41 @@ const Competition = () => {
       },
     },
   };
+  const [data, setData] = React.useState([]);
+  const fetchCompetitions = async () => {
+    try {
+      const dataApi = await competitionApi.getAllCompetition();
+      setData(dataApi);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchCompetitions();
+  }, []);
+
   return (
-    <div id="cuoc-thi" className="competition-container">
-      <div className="competition-title">
+    <div id='cuoc-thi' className='competition-container'>
+      <div className='competition-title'>
         <h1>CUỘC THI</h1>
       </div>
-      <OwlCarousel
-        className="owl-theme owl-carousel__competition"
-        {...options}
-        // loop
-        // margin={0}
-        // items="1"
-        // stagePadding={175}
-      >
-        <div className="item">
-          <img src="https://i.pinimg.com/564x/94/9b/8d/949b8d8d9229693ba9d53b054b738e2a.jpg" />
-        </div>
-        <div className="item">
-          <img src="https://i.pinimg.com/originals/51/81/2a/51812ae2714816f7b2b094edeeba0041.jpg" />
-        </div>
-        <div className="item">
-          <img src="https://img4.thuthuatphanmem.vn/uploads/2019/11/17/anh-giot-nuoc-dep-nhat_113939355.jpg" />
-        </div>
-        <div className="item">
-          <img src="https://i.pinimg.com/564x/1b/3a/b6/1b3ab6c2b28e891d94bc49aa9e10841b.jpg" />
-        </div>
-        <div className="item">
-          <img src="https://i.pinimg.com/564x/94/9b/8d/949b8d8d9229693ba9d53b054b738e2a.jpg" />
-        </div>
-        <div className="item">
-          <img src="https://i.pinimg.com/564x/94/9b/8d/949b8d8d9229693ba9d53b054b738e2a.jpg" />
-        </div>
-      </OwlCarousel>
+      {data?.length > 0 ? (
+        <OwlCarousel
+          className='owl-theme owl-carousel__competition'
+          {...options}
+        >
+          {data?.map((item) => (
+            <div key={item.id} className='item'>
+              <img
+                src={`http://127.0.0.1:1111/public/images/competition/${item.portrait_poster}`}
+              />
+            </div>
+          ))}
+        </OwlCarousel>
+      ) : (
+        <span>Loading...</span>
+      )}
     </div>
   );
 };
--- font-end/src/components/homepage/Donor.js
+++ font-end/src/components/homepage/Donor.js
@@ -1,69 +1,92 @@
 import React from "react";
+import sponsorApi from "../../api/sponsorApi";
 import "./donor.scss";
 const Donor = () => {
+  const [data, setData] = React.useState([]);
+  const fetchSponsors = async () => {
+    try {
+      const dataApi = await sponsorApi.getAllsponsor();
+      setData(dataApi);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchSponsors();
+  }, []);
+
   return (
-    <div className="donor">
-      <h1 className="donor-title">ĐỐI TÁC ĐỒNG HÀNH</h1>
-      <div className="donor-logo">
+    <div className='donor'>
+      <h1 className='donor-title'>ĐỐI TÁC ĐỒNG HÀNH</h1>
+      <div className='donor-logo'>
+        {data?.map((item, index) => (
+          <img
+            key={item.id}
+            className={`donor-logo_${index + 1}`}
+            src={`http://127.0.0.1:1111/public/images/sponsor/${item.logo}`}
+            alt={item.name}
+          />
+        ))}
         <img
-          className="donor-logo_1"
-          src="/img/Donor/nashtech.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_1'
+          src='/img/Donor/nashtech.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_2"
-          src="/img/Donor/edu2review.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_2'
+          src='/img/Donor/edu2review.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_3"
-          src="/img/Donor/momo.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_3'
+          src='/img/Donor/momo.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_4"
-          src="/img/Donor/coffeeandtea.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_4'
+          src='/img/Donor/coffeeandtea.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_5"
-          src="/img/Donor/intership.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_5'
+          src='/img/Donor/intership.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_6"
-          src="/img/Donor/camhouse.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_6'
+          src='/img/Donor/camhouse.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_7"
-          src="/img/Donor/infinity.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_7'
+          src='/img/Donor/infinity.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_8"
-          src="/img/Donor/topcv.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_8'
+          src='/img/Donor/topcv.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_9"
-          src="/img/Donor/diginet.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_9'
+          src='/img/Donor/diginet.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_10"
-          src="/img/Donor/scommunications.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_10'
+          src='/img/Donor/scommunications.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_11"
-          src="/img/Donor/uehinstitute.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_11'
+          src='/img/Donor/uehinstitute.png'
+          alt='logo đối tác đồng hành'
         />
         <img
-          className="donor-logo_12"
-          src="/img/Donor/unknow.png"
-          alt="logo đối tác đồng hành"
+          className='donor-logo_12'
+          src='/img/Donor/unknow.png'
+          alt='logo đối tác đồng hành'
         />
       </div>
     </div>
